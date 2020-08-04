7758214363d6091513cc463ca7e3f7a0384dfea7
desni12 <luhdesni38@gmail.com>
2020-08-04 16:15:29 +0800

    commit pertama

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\News;
+use Illuminate\Http\Request;
+
+class NewsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Berita::all();
+
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = Berita::create($request->all());
+        return "data telah berhasil disimpan";
+
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\News  $news
+     * @return \Illuminate\Http\Response
+     */
+    public function show(News $news)
+    {
+        $data=Berita::findOrFail();
+        return $data;
+
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\News  $news
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(News $news)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\News  $news
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, News $news)
+    {
+        $data=Berita::findOrFail($id);
+        $data->tanggal=$request->tanggal;
+        $data->judul=$request->judul;
+        $data->headline=$request->headline;
+        $data->isi=$request->isi;
+        $data->penulis=$request->penulis;
+        $data->save();
+        return"data berhasil diubah";
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\News  $news
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(News $news)
+    {
+        $data=Berita::findOrFail($id);
+        $data->delete();
+        return"data berhasil dihapus";
+        //
+    }
+}
